4f712d47da62966602659bb377d60fb7d5b57d33
varunkothamachu <varunkothamachu@gmail.com>
2018-08-03 15:54:27 +0100

    Added script to compare simulation data from Non Identical Inducer Exchange Model with experimental data

---
+++ AMIGO2R2016d/Examples/Toggle_switch/Lugagne_model/Comparing_lugagne_predictions_to_exp_Data/Draw_comparison_plots_NIEM.m
@@ -0,0 +1,33 @@
+function [SSE_LacI,SSE_TetR] = Draw_comparison_plots(EXP_data,results,inputs)
+% calculate SSE to compare simulation data with expeimental data from
+% Lugagne paper
+
+
+SSE_LacI=[];
+SSE_TetR=[];
+
+for i=1:inputs.exps.n_exp
+
+    figure(1)
+    subplot(6,1,i)
+    % plot(EXP_data{1,i}.timerfp/3600,EXP_data{1,i}.meanRFP,'r--+'); hold on;
+    plot(results.sim.tsim{1,i}./60,results.sim.states{1,i}(:,5),'red'); hold on;
+    title(strcat('calibration-',num2str(i+1)))
+    xlabel('time(hours)')
+
+    %figure(2)
+    %subplot(2,3,i)
+    %plot(EXP_data{1,i}.timeGFP/3600,EXP_data{1,i}.meanGFP,'g--+'); hold on;
+    plot(results.sim.tsim{1,i}./60,results.sim.states{1,i}(:,6),'green'); hold on;
+    title(strcat('calibration-',num2str(i+1)))
+    xlabel('time(hours)')
+
+
+    %% SSE calculation
+
+    SSE_LacI=[SSE_LacI,sum((EXP_data{1,i}.meanGFP-results.sim.states{1,i}(:,5)).^2)]; %#ok<*AGROW>
+    SSE_TetR=[SSE_TetR,sum((EXP_data{1,i}.meanRFP-results.sim.states{1,i}(:,6)).^2)];
+
+end
+
+end
